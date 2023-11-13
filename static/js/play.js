b7de90e91e664b2a6eae9d3ae614cf5f2ccf9bb3
Kunal-JockL <kunalsalvak@gmail.com>
2023-11-13 22:04:08 +0530

    play page front end content-1 is done (i think)

--- static/js/play.js
+++ static/js/play.js
@@ -12,6 +12,18 @@ $(document).ready(function() {
     socket.on('joinLobby', (data) => {
         console.log('joined lobby ' + data);
     });
-    
+
+    const readyButton = document.getElementById('ready-button');
+    const player = document.getElementById('player1');
+
+    readyButton.addEventListener('click', function() {
+        player.style.color = 'yellow';
+        player.style.transform = 'scale(1.2)';
+        readyButton.style.backgroundColor = '#ffffff';
+        readyButton.style.color = 'rgb(176,176,176)';
+        
     });
+    
+    
+});
     
