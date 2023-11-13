b9f92d281781bc880cbe806b0193e20b13655ac2
Ian Tauro <sushruth127@gmail.com>
2023-11-13 23:23:08 +0530

    began integration of front end with back-end

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const path = require('path');
 require('dotenv').config();
 
 
-// If you dont have the .env file (since it is in .gitignore), create a .env file and set port and host to what you wish.
+// If you dont have the .env file (since it is in .gitignore), create a .env file and set port to what you wish.
 const PORT = process.env.PORT;
-const HOST = process.env.HOST;
 
 const app = express();
 
@@ -22,8 +21,7 @@ app.set('view engine','ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 
-// GET routes to render appropriate screen. Redirected to wait and play through the socket.io room.
-// Need to not allow the user to directly put http://localhost:58617/wait or http://localhost:58617/roomkey/play and access the page.
+// GET routes to render index.ejs
 app.get('/', (req, res) => {
     res.render('index', { roomKey: undefined, full: false });
 });
@@ -40,14 +38,17 @@ app.get('/:roomKey/play', (req, res) => {
     }
 });
 
+
 // POST routes
 app.post('/', (req, res) => {
     const roomKey = req.body.roomKey;
     const username = req.body.username;
     const roomToJoin = roomsList.find((room) => room.roomKey === roomKey);
 
-    if(roomToJoin === undefined || roomToJoin.users.length == 1) {
-        res.render('play', { roomKey: roomKey, username: username });
+    if(roomToJoin === undefined) {
+        res.render('play', { roomKey: roomKey, username: username, enemyUsername: '...' });
+    } else if(roomToJoin.users.length == 1) {
+        res.render('play', { roomKey: roomKey, username: username, enemyUsername: roomToJoin.users[0] });
     } else {
         res.render('index', { roomKey: undefined, full: true });
     }
@@ -69,9 +70,13 @@ io.on('connection', (socket) => {
             roomKey: roomKey,
             users: [username],
         });
+
+        console.log(roomsList[roomsList.length - 1]);
     } else if (io.sockets.adapter.rooms.get(roomKey).size === 1) {
         const roomToJoin = roomsList.find((room) => room.roomKey === roomKey);
         roomToJoin.users.push(username);
+        
+        console.log(roomToJoin);
     }
     
     socket.join(roomKey);
@@ -82,8 +87,6 @@ io.on('connection', (socket) => {
 
         const roomIndex = roomsList.findIndex(room => room.roomKey === roomKey);
 
-        console.log(roomsList[roomIndex]);
-
         if(roomsList[roomIndex].users.length === 1) {
             console.log(`[-] Room [${roomKey}] was destroyed!`)
             roomsList.splice(roomIndex, 1);
@@ -92,12 +95,14 @@ io.on('connection', (socket) => {
             if(roomsList[roomIndex].users[0] != username) { userIndex = 1; }
 
             roomsList[roomIndex].users.splice(userIndex, 1);
+            
+            console.log(roomsList[roomIndex]);
         }
         
+        io.sockets.in(roomKey).emit('lobbyUpdate', roomsList.find((room) => room.roomKey === roomKey));
     });
 
-    socket.emit('joinLobby', roomKey);
-    //io.sockets.in(roomKey).emit('test', users);
+    io.sockets.in(roomKey).emit('lobbyUpdate', roomsList.find((room) => room.roomKey === roomKey));
 });
 
 
--- static/js/play.js
+++ static/js/play.js
@@ -8,22 +8,57 @@ $(document).ready(function() {
             roomKey: roomKey
         }
     });
-    
-    socket.on('joinLobby', (data) => {
-        console.log('joined lobby ' + data);
+
+    socket.on('lobbyUpdate', (roomData) => {
+        if(roomData.users.length === 2) {
+            var enemyuser = roomData.users[0];
+            if(enemyuser === username) {
+                enemyuser = roomData.users[1];
+            }
+            $('#enemy').html(enemyuser);
+        }
+        else {
+            $('#enemy').html('...');
+        }
     });
 
-    const readyButton = document.getElementById('ready-button');
-    const player = document.getElementById('player1');
+    const readyButton = $('#ready-button');
+    const you = $('#you');
+    const enemy = $('#enemy');
+    var ready = false;
 
-    readyButton.addEventListener('click', function() {
-        player.style.color = 'yellow';
-        player.style.transform = 'scale(1.2)';
-        readyButton.style.backgroundColor = '#ffffff';
-        readyButton.style.color = 'rgb(176,176,176)';
-        
-    });
+    readyButton.click(() => {
+        if(!ready) {
+            $('#ready-count').html('1');
+
+            you.css({
+                "color": "yellow",
+                "transform": "scale(1.2)"
+            });
     
+            readyButton.css({
+                "background-color": "#ffffff",
+                "color": "#b0b0b0"
+            });
+
+            ready = true;
+        }
+        else {
+            $('#ready-count').html('0');
+
+            you.css({
+                "color": "#b0b0b0",
+                "transform": "scale(1)"
+            });
+
+            readyButton.css({
+                "background-color": "#b0b0b0",
+                "color": "#ffffff"
+            });
+
+            ready = false;
+        }
+    });
     
 });
     
