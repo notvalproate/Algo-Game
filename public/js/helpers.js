796c570e9269bd1512cdf8303848aefc3fdd2f48
Ian Tauro <sushruth127@gmail.com>
2023-11-29 02:37:57 +0530

    Added confetti on win and loss

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -195,6 +195,51 @@ function removeHighlightFrom(cardDiv) {
     $(cardDiv).removeClass('selected');
 }
 
+function displayConfetti(wonGame) {
+    const end = Date.now() + 8 * 1000;
+    let colors = ["#ffffff"];
+    let originY = 0;
+    let angleL = -30;
+    let angleR = -150;
+
+    if(wonGame) {
+        colors.push("#00bb00");
+        colors.push("#e0c331");
+    } else {
+        colors.push("#bb0000");
+        colors.push("#ff7575");
+        originY = 0.9;
+        angleL = 60;
+        angleR = 120;
+    }
+    
+    (function frame() {
+      confetti({
+        zIndex: 999,
+        particleCount: 2,
+        angle: angleL,
+        spread: 60,
+        origin: { x: 0, y: originY },
+        colors: colors,
+        startVelocity: 90,
+      });
+    
+      confetti({
+        zIndex: 999,
+        particleCount: 2,
+        angle: angleR,
+        spread: 60,
+        origin: { x: 1, y: originY },
+        colors: colors,
+        startVelocity: 90,
+      });
+    
+      if (Date.now() < end) {
+        requestAnimationFrame(frame);
+      }
+    })();
+}
+
 function showResultModal(wonGame) {
     if(wonGame) {
         gameResult.html('YOU WIN');
@@ -206,122 +251,8 @@ function showResultModal(wonGame) {
 
     setTimeout(() => {
         resultModal.addClass('modal-in');
+        displayConfetti(wonGame);
     }, 1600);
-}
-
-function runParticlesJs() {
-    particlesJS('particles-js',
-        {
-            "particles": {
-                "number": {
-                  "value": 48,
-                  "density": {
-                    "enable": true,
-                    "value_area": 394.57382081613633
-                  }
-                },
-                "color": {
-                  "value": "#ffffff"
-                },
-                "shape": {
-                  "type": "triangle",
-                  "stroke": {
-                    "width": 0,
-                    "color": "#000000"
-                  },
-                  "polygon": {
-                    "nb_sides": 3
-                  },
-                  "image": {
-                    "src": "img/github.svg",
-                    "width": 100,
-                    "height": 100
-                  }
-                },
-                "opacity": {
-                  "value": 0.6234266368894954,
-                  "random": false,
-                  "anim": {
-                    "enable": false,
-                    "speed": 1,
-                    "opacity_min": 0.1,
-                    "sync": false
-                  }
-                },
-                "size": {
-                  "value": 0,
-                  "random": true,
-                  "anim": {
-                    "enable": false,
-                    "speed": 2,
-                    "size_min": 0,
-                    "sync": false
-                  }
-                },
-                "line_linked": {
-                  "enable": true,
-                  "distance": 157.82952832645452,
-                  "color": "#ffffff",
-                  "opacity": 0.2367442924896818,
-                  "width": 1
-                },
-                "move": {
-                  "enable": true,
-                  "speed": 1,
-                  "direction": "none",
-                  "random": false,
-                  "straight": false,
-                  "out_mode": "out",
-                  "bounce": false,
-                  "attract": {
-                    "enable": false,
-                    "rotateX": 600,
-                    "rotateY": 1200
-                  }
-                }
-              },
-              "interactivity": {
-                "detect_on": "canvas",
-                "events": {
-                  "onhover": {
-                    "enable": false,
-                    "mode": "repulse"
-                  },
-                  "onclick": {
-                    "enable": false,
-                    "mode": "push"
-                  },
-                  "resize": true
-                },
-                "modes": {
-                  "grab": {
-                    "distance": 400,
-                    "line_linked": {
-                      "opacity": 1
-                    }
-                  },
-                  "bubble": {
-                    "distance": 400,
-                    "size": 40,
-                    "duration": 2,
-                    "opacity": 8,
-                    "speed": 3
-                  },
-                  "repulse": {
-                    "distance": 200,
-                    "duration": 0.4
-                  },
-                  "push": {
-                    "particles_nb": 4
-                  },
-                  "remove": {
-                    "particles_nb": 2
-                  }
-                }
-              },
-              "retina_detect": true,
-        }
-    );
 }
 
 export {
@@ -345,6 +276,4 @@ export {
     removeHighlightFrom,
 
     showResultModal,
-
-    runParticlesJs,
 }
