05f1f3d3b16fd79669fe3582496343b51fd63ad5
Ian Tauro <sushruth127@gmail.com>
2023-12-01 15:52:42 +0530

    Finished create lobby page

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,27 @@
 $(document).ready(function() {
-    $('header').removeClass('header-out');
-    $('footer').removeClass('footer-out');
-    $('.login').addClass('fade-in');
     $('.main-bg').addClass('fade-in');
+
+    setTimeout(() => {
+        $('header').removeClass('header-out');
+        $('footer').removeClass('footer-out');
+    }, 1000);
+
+    setTimeout(() => {
+        $('.login').addClass('fade-in');
+    }, 1400);
+});
+
+$('#playButton').on('click', (event) => {
+    event.preventDefault();
+
+    $('.login').removeClass('fade-in');
+
+    setTimeout(() => {
+        $('header').addClass('header-out');
+        $('footer').addClass('footer-out');
+    }, 400);
+    
+    setTimeout(() => {
+      $('#playForm').submit();
+    }, 1500);
 });
