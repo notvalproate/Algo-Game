839464a2f8b4e90a196ccc14e9c7e1882e55a1af
Ian Tauro <sushruth127@gmail.com>
2023-12-11 19:49:39 +0530

    Removed webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const TerserPlugin = require("terser-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
     mode: 'production',
@@ -12,21 +10,9 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    module: {
-        rules: [
-          {
-            test: /\.css$/,
-            exclude: /node_modules/,
-            use: [MiniCssExtractPlugin.loader, "css-loader"],
-          },
-        ],
-    },
     optimization: {
         minimize: true,
-        minimizer: [
-            new TerserPlugin(),
-            new CssMinimizerPlugin(),
-        ],
+        minimizer: [new TerserPlugin()],
     },
-    plugins: [new TerserPlugin(), new MiniCssExtractPlugin()],
+    plugins: [new TerserPlugin()],
 };
